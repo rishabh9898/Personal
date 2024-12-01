3c7b4ebbf82b9ec1cef271d8a35c54534f4f9218
rishabh9898 <72998580+rishabh9898@users.noreply.github.com>
2024-11-30 20:30:00 -0800

    Time: 1216 ms (62.88%) | Memory: 65.1 MB (23.19%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -0,0 +1,45 @@
+class TrieNode:
+
+    def __init__(self):
+        self.children = {}
+        self.endofword = False
+        
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+
+        curr = self.root
+
+        for i in word:
+            if i not in curr.children:
+                curr.children[i] = TrieNode()
+            curr = curr.children[i]
+        
+        curr.endofword = True
+    
+    def search(self, word: str) -> bool:
+        def dfs(j,root):
+
+            curr = root
+
+            for i in range(j,len(word)):
+                if word[i]== ".":
+                    for child in curr.children.values():
+                        if dfs(i+1,child):
+                            return True
+                    return False
+                    
+                else:
+                    if word[i] not in curr.children:
+                        return False
+                    curr = curr.children[word[i]]
+            
+            return curr.endofword
+        
+        return dfs(0,self.root)
+        
