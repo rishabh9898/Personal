44a39e1990f7691badd682a40d740ae99b521181
rishabh9898 <72998580+rishabh9898@users.noreply.github.com>
2024-12-08 19:04:42 -0700

    Time: 3 ms (52.73%) | Memory: 17.2 MB (14.68%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -2,41 +2,44 @@ class Solution:
     def reorganizeString(self, s: str) -> str:
 
         dict = {}
-        
-        for i in s:
-            if i in dict:
-                dict[i] +=1
-            else:
-                dict[i] = 1
-        
-        #  we have characters stored 
-
-        maxHeap = [ [-1*cnt,char] for char,cnt in dict.items()]
-
-        heapq.heapify(maxHeap)
-
-
-        prev = None
-
         res = ""
 
-        while prev or maxHeap:
+        for i in s:
+            if i in dict:
+                dict[i]+=1
+            else:
+                dict[i]=1
+               
+        #  Stored the frequency
 
+        maxHeap = []
+
+        for char,cnt in dict.items():
+            maxHeap.append([-1*cnt,char])
+        
+        heapq.heapify(maxHeap)
+        prev = None
+
+        while maxHeap or prev:
             if prev and not maxHeap:
                 return ""
-
-            count,char = heapq.heappop(maxHeap)
-            res +=char
-            count+=1
-
+            cnt,char = heapq.heappop(maxHeap)
+            res+=char
+            cnt+=1
             if prev:
                 heapq.heappush(maxHeap, prev)
                 prev = None
 
-            if count!=0:
-                prev = [count,char]
+            if cnt!=0:
+                prev = [cnt,char]
 
         return res
+            
+
+
+
+
+
 
             
 
